e2fba8a5aef8e7e6da26b8183c7b2f6fe6ddcde7
Ivan Pereira <navi1921@gmail.com>
2019-10-07 01:14:48 -0300

    Update utils2.py

--- utils2.py
+++ utils2.py
@@ -234,22 +234,25 @@ def get_data(dataset, data_path,val1_data_path,val2_data_path, cutout_length, va
         one_hot_y_train = []
         one_hot_y_test = []
         for i in range(len(y_train)):
-            one_hot_y = np.zeros(num_classes,dtype=object)
+            #one_hot_y = np.zeros(num_classes)
+            one_hot_y = torch.tensor([0, 0, 0])
             one_hot_y[y_train[i]] = 1
             one_hot_y_train.append(one_hot_y)
         for i in range(len(y_test)):
-            one_hot_y =np.zeros(num_classes,dtype=object)
+            #one_hot_y =np.zeros(num_classes)
+            one_hot_y = torch.tensor([0, 0, 0])
             one_hot_y[y_test[i]] = 1
             one_hot_y_test.append(one_hot_y)
             
         print(y_train)
         tensor_train_x = torch.stack([torch.Tensor(i) for i in x_train_data]) # transform to torch tensors
-        tensor_train_y = torch.from_numpy(np.asarray(one_hot_y_train))
-        tensor_test_y = torch.from_numpy(np.asarray(one_hot_y_test))
-        
-        #tensor_train_y = torch.stack([torch.Tensor(i) for i in one_hot_y_train])
         tensor_test_x = torch.stack([torch.Tensor(i) for i in x_test_data]) # transform to torch tensors
-        #tensor_test_y = torch.stack([torch.Tensor(i) for i in one_hot_y_test])
+        
+        #tensor_train_y = torch.from_numpy(np.asarray(one_hot_y_train))
+        #tensor_test_y = torch.from_numpy(np.asarray(one_hot_y_test))
+        
+        tensor_train_y = torch.stack([torch.Tensor(i) for i in one_hot_y_train])
+        tensor_test_y = torch.stack([torch.Tensor(i) for i in one_hot_y_test])
         
         import torch.utils.data as utils
 
