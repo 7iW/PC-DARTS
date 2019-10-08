6f735db3796f1c9b6de5c90f2af259703a59869c
Ivan Pereira <navi1921@gmail.com>
2019-10-08 01:05:51 -0300

    Update utils3.py

--- utils3.py
+++ utils3.py
@@ -119,7 +119,7 @@ def get_data(dataset, data_path,val1_data_path,val2_data_path, cutout_length, va
             one_hot_y_test.append(one_hot_y)
             
         tensor_train_x = torch.stack([torch.Tensor(i.type('torch.FloatTensor')) for i in x_train_data]) # transform to torch tensors
-        tensor_test_x = torch.stack([torch.Tensor(i) for i in x_test_data]) # transform to torch tensors
+        tensor_test_x = torch.stack([torch.Tensor(i.type('torch.FloatTensor')) for i in x_test_data]) # transform to torch tensors
         #tensor_train_x = torch.stack([i.type('torch.DoubleTensor') for i in x_train_data])
         #tensor_test_x = torch.stack([i.type('torch.DoubleTensor') for i in x_test_data]) 
         #tensor_train_y = torch.from_numpy(np.asarray(one_hot_y_train))
