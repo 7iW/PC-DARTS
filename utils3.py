a3597e4ff541f737926262eaf990f834c08a653e
Ivan Pereira <navi1921@gmail.com>
2019-10-08 00:23:09 -0300

    Update utils3.py

--- utils3.py
+++ utils3.py
@@ -43,6 +43,7 @@ def load(path):
   Xs,Ys = [],[]
   with open(path) as fp:
     for line in fp:
+        print(line.split(','))
         name,label = line.split(',')
         Xs.append(name)
         Ys.append(int(label))
