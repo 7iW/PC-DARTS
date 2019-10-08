97afc0f41be63d37f5a5eff485a79f895ffc9463
Ivan Pereira <navi1921@gmail.com>
2019-10-08 00:40:30 -0300

    Update utils3.py

--- utils3.py
+++ utils3.py
@@ -80,12 +80,12 @@ def get_data(dataset, data_path,val1_data_path,val2_data_path, cutout_length, va
         x_train_data = []
         x_test_data = []
         for x_path in X_train:
-            x = cv2.imread(data_path+x_path, cv2.IMREAD_GRAYSCALE) 
+            x = cv2.imread(data_path+x_path) 
             #x_re = cv2.resize(x,(image_size,image_size))
             #rgb = cv2.merge([x_re,x_re,x_re])
             x_train_data.append(x)
         for x_path in X_test:
-            x = cv2.imread(data_path+x_path, cv2.IMREAD_GRAYSCALE) 
+            x = cv2.imread(data_path+x_path) 
            
             #x_re = cv2.resize(x,(image_size,image_size))
             #rgb = cv2.merge([x_re,x_re,x_re])
