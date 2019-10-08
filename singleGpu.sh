9d6a654fe3cf4e13c830c253e494238095f373a1
Ivan Pereira <navi1921@gmail.com>
2019-10-08 11:59:41 -0300

    Update singleGpu.sh

--- singleGpu.sh
+++ singleGpu.sh
@@ -1,3 +1,3 @@
 
 #No diretorio acima ao train_data_path é necessario ter um labels.txt
-python train_search_big_paral.py  --image_size 256  --train_data_path '/content/dataset/data/' --epochs 300 --n_class 5 --batch_size 16
+python train_search_big_paral.py  --image_size 256  --data_path 'data/' --labels_path 'labels.txt' --epochs 300 --n_class 5 --batch_size 16
